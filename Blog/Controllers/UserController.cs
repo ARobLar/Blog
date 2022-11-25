ea260e7b87d9b8d61d9f8d831a9b5df28f10c55b
ARobLar <a.robin.larsson@gmail.com>
2022-11-25 13:01:49 +0100

    Added Dto for user data and controller with stubs for user api calls

---
+++ Blog/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Blog.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("card/{username}")]
+        public UserDto GetUserCard(string username)
+        {
+            return new UserDto();
+        }
+        [HttpGet("cards")]
+        public IEnumerable<UserCardDto> GetUserCards() 
+        {
+            return new List<UserCardDto>();
+        }
+
+        [HttpPost("create")]
+        public bool Create([FromBody] UserDto user)
+        {
+            return true;
+        }
+
+        [HttpDelete("delete/{usedId}")]
+        public bool DeleteAccount(int userId)
+        {
+            return true;
+        }
+    }
+}
---
+++ Blog/Dto/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog.Dto
+{
+    public class UserDto
+    {
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
