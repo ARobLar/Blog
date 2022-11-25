8df68c021d4d28629c3123571a3353233d2f09a6
ARobLar <a.robin.larsson@gmail.com>
2022-11-25 10:00:06 +0100

    Added sqlite database with

---
+++ Blog/Entities/BlogDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Controllers
+{
+    public class BlogDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data Source=C:\Users\brittleSmallBrick\source\repos\Blog_NextjsRedux_DotNetCore\Blog\Blog.db");
+    }
+}
