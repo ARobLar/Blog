32d5aec9a21a8613ec28439403d32bd839a38f3a
ARobLar <a.robin.larsson@gmail.com>
2022-11-28 03:40:53 +0100

    set up server side with endpoints and dummy data for signing in and signing out

---
+++ Blog/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Blog.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("signIn")]
+        public UserDto SignInUser([FromBody] signInDto credentials)
+        {
+            return new UserDto
+            {
+                Id = "4",
+                Username = "User4",
+                Email = "Test",
+                Role = "Admin"
+            };
+        }
+
+        [HttpPost("signOut/{username}")]
+        public Task<bool> SignOutUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
