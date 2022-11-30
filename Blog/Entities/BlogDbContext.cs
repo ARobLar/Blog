9ebf82eefb42b00a34578f7205c92b64faf7c68a
ARobLar <a.robin.larsson@gmail.com>
2022-12-01 00:51:17 +0100

    Put database connection string appsettings

--- Blog/Entities/BlogDbContext.cs
+++ Blog/Entities/BlogDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Blog.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace Blog.Controllers
 {
@@ -8,7 +10,19 @@ namespace Blog.Controllers
     {
         public DbSet<PostEntity> Posts { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite(@"Data Source=C:\Users\brittleSmallBrick\source\repos\Blog_NextjsRedux_DotNetCore\Blog\Blog.db");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.GetConnectionString("database");
+                optionsBuilder.UseSqlite(connectionString);
+            }
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //    => options.UseSqlite(@"Data Source=C:\Users\brittleSmallBrick\source\repos\Blog_NextjsRedux_DotNetCore\Blog\Blog.db");
     }
 }
