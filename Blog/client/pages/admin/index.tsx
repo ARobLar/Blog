0666b65b8d4163b51b7569444ce280f087ad19ef
ARobLar <a.robin.larsson@gmail.com>
2022-11-30 01:15:35 +0100

    added admin page with list of users

---
+++ Blog/client/pages/admin/index.tsx
@@ -0,0 +1,8 @@
+import UserTable from "../../src/components/admin/UserTable";
+
+export default function Admin(){
+
+  return(
+    <UserTable/>
+  );
+}
---
+++ Blog/client/src/components/admin/UserTable/index.tsx
@@ -0,0 +1,21 @@
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import React from "react";
+import { blogUsersInfo } from "../../../../mockData/blogUsers";
+import UserTableList from "./UserTableList";
+
+var users = blogUsersInfo;
+
+export default function UserTable() {
+
+    return(
+        <div>
+            <Container maxWidth="lg">
+                <Typography>
+                    Active Accounts
+                </Typography>
+                <UserTableList users={users}/>
+            </Container>
+        </div>
+    );
+}
