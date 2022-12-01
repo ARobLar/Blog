645e2f2b2a32af55e3efbcc00937b33c5f9d22f8
ARobLar <a.robin.larsson@gmail.com>
2022-12-01 09:04:32 +0100

    only admins can access admin page, unauthorized is routed back to previous page

--- Blog/client/pages/admin/index.tsx
+++ Blog/client/pages/admin/index.tsx
@@ -1,20 +1,27 @@
+import { useRouter } from "next/router";
 import { useGetCurrentUserQuery } from "../../src/api/apiSlice";
 import RegistrationForm from "../../src/components/admin/RegistrationForm";
 import UserTable from "../../src/components/admin/UserTable";
 
 export default function Admin(){
+  const router = useRouter();
   const {data: user, isFetching, isSuccess} = useGetCurrentUserQuery();
-  
+
   if(isFetching){
     return(<h1>Loading..</h1>)
   }
 
-  if(isSuccess && user && user.role == "Admin"){
+  if(isSuccess){
+
+    if(!user || user.role != "Admin"){
+      router.back();
+    }
+
     return(
       <>
-      <RegistrationForm/>
-      <UserTable/>
-    </>
+        <RegistrationForm/>
+        <UserTable/>
+      </>
     );
   }
 }
