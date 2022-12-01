abbbe5041c9d25ac8a4a185c8a25b1caf7b4a936
ARobLar <a.robin.larsson@gmail.com>
2022-12-01 06:16:22 +0100

    adjusted page styling on showing blog post

--- Blog/client/pages/[username]/post/show/[id].tsx
+++ Blog/client/pages/[username]/post/show/[id].tsx
@@ -5,26 +5,40 @@ import Box from '@mui/material/Box';
 import { useGetPostQuery } from '../../../../src/api/apiSlice';
 import CircularProgress from '@mui/material/CircularProgress';
 import { useRouter } from 'next/router';
-import theme from '../../../../src/theme';
 import { hostBaseUrl } from '../../../../src/CONSTANTS';
+import Stack from '@mui/material/Stack';
+import theme from '../../../../src/theme';
 
 const useStyles = makeStyles({
- postTitle: {
-  minHeight: "120px",
-  backgroundPosition: "center",
-  backgroundRepeat: "no-repeat",
-  backgroundSize: "cover",
-  position: "relative",
+ main: {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  color: "#000",
-  fontSize: "3em",
-  [theme.breakpoints.down("sm")]: {
-    minHeight: "80px",
-    fontSize: "2em"
+ },
+ postTitle: {
+  minHeight: "10vw",
+  display: "flex",
+  alignItems: "center",
+  fontSize: "5vw",
+ },
+ image: {
+  maxHeight: "20vw",
+  maxWidth: "40vw",
+  marginBottom: "20px",
+ },
+ date: {
+  fontSize: "2vw",
+  [theme.breakpoints.down("md")]: {
+    fontSize: "3vw"
   }
  },
+ text: {
+  fontSize: "16px",
+  margin: "0 10px",
+  [theme.breakpoints.down("md")]: {
+    fontSize: "12px"
+  }
+ }
 })
 
 export default function DisplayPost() {
@@ -47,18 +61,17 @@ export default function DisplayPost() {
     const date = (new Date(Date.parse(post.creationTime.toLocaleString())));
 
     return ( 
-      <div>
-        <Typography variant="h3" component="h2" className={classes.postTitle}>
+      <Stack className={classes.main}>
+        <Typography className={classes.postTitle}>
           {post.title}
         </Typography>
-        <img src={`${hostBaseUrl}/${post.imageSource}`} alt={post.imageLabel} width="100%" />
-        <Typography variant="body1" component="h2" color="textSecondary">
+        <img className={classes.image} src={`${hostBaseUrl}/${post.imageSource}`} alt={post.imageLabel} width="100%" />
+        <Typography className={classes.date} color="textSecondary">
           Created: {date.toLocaleDateString()} at {date.toLocaleTimeString()}
         </Typography>
-        <Divider/>
-        <Typography variant="body2" component="h2" color="textPrimary">
+        <Typography className={classes.text} color="textPrimary">
           {post.text}
         </Typography>
-      </div>
+      </Stack>
   )}
 }
