6679793639fab198fbd0da3c8dc571a44630763c
ARobLar <a.robin.larsson@gmail.com>
2022-12-05 17:13:40 +0100

    styles for form component, missed for previous commit

---
+++ Blog/client/src/styles/formStyles.ts
@@ -0,0 +1,18 @@
+import { makeStyles } from "@mui/styles";
+import theme from "../theme";
+
+export const useFormStyles = makeStyles({
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  form: {
+    width: "50%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+});
